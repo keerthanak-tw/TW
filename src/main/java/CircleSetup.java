37a1e2f3bf49d4cff4924847e501f123fe5b7a69
keerthanak-tw <76804118+keerthanak-tw@users.noreply.github.com>
2020-12-31 13:36:24 +0530

    Add files via upload

---
+++ src/main/java/CircleSetup.java
@@ -0,0 +1,15 @@
+import processing.core.PApplet;
+public class CircleSetup {
+    public final int height;
+    public final int speed;
+    public final int diameter;
+    public int x_pos;
+
+    public CircleSetup(int height, int speed, int diameter) {
+        this.height = height;
+        this.speed = speed;
+        this.diameter = diameter;
+        this.x_pos = 0;
+    }
+
+}
