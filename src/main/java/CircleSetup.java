00c9e5f645ba5e056466a064ae749cb42ef95a65
keerthanak-tw <76804118+keerthanak-tw@users.noreply.github.com>
2021-01-03 10:05:15 +0530

    Update CircleSetup.java

--- src/main/java/CircleSetup.java
+++ src/main/java/CircleSetup.java
@@ -3,13 +3,13 @@ public class CircleSetup {
     public final int height;
     public final int speed;
     public final int diameter;
-    public int x_pos;
+    public int position;
 
     public CircleSetup(int height, int speed, int diameter) {
         this.height = height;
         this.speed = speed;
         this.diameter = diameter;
-        this.x_pos = 0;
+        this.position = 0;
     }
 
 }
